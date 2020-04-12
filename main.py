33b1dd97664e426402394056d81e98d0599fcb58
Roger Wang <wang.rroger@gmail.com>
2020-04-12 18:15:32 -0400

    scraping from webpage itself as well, instead of just looking for PDFs

--- main.py
+++ main.py
@@ -12,26 +12,27 @@ ROOT_PDFS_PATH = 'pdfs/'
 
 def main():
 
+    inputTSVFileName = 'part_91_papers_havelink.tsv'
     # Download PDFs
-    pdfList = download_pdfs()
-
-    # Populate list of PDFs to parse
-    #pdfList = get_pdf_paths()
+    researchPaperData = getDataForProvidedTSV(inputTSVFileName)
 
     # Print our results
-    # print('#############################################')
-    # print('Final Results')
-    # for pdf in pdfList:
-    #     print(pdf)
+    print('#############################################')
+    with open('results.tsv', 'wt') as out_file:
+        tsv_writer = csv.writer(out_file, delimiter='\t')
+        tsv_writer.writerow(['PaperID', 'Emails'])
+        for id, researchPaperDatum in researchPaperData.items():
+            tsv_writer.writerow([id, researchPaperDatum['emails']])
+    print('Results written to results.tsv')
 
 
-def download_pdfs():
+def getDataForProvidedTSV(inputTSVFileName):
     print('Downloading...')
     alreadyDownloadedPDFs = get_pdf_paths()
     researchPaperData = {}
 
     # Start building dict of all the associated PDFs for each research paper in the given tsv...
-    with open("part_91_papers_havelink.tsv") as tsv:
+    with open(inputTSVFileName) as tsv:
         reader = csv.reader(tsv, delimiter="\t", quotechar='"')
         for row in reader:
             #column 2 is where the link is
@@ -47,67 +48,85 @@ def download_pdfs():
 
     # Map research paper ID and the downloaded PDF paths
     for id,datum in researchPaperData.items():
-        if id == 'se62cx9w':
-            print('--------------------------------------------')
-            print('For research paper ID: ' + id)
+        # if id == 'ljzmei78':
+        print('--------------------------------------------')
+        print('For research paper ID: ' + id)
 
-            pdfFilePath = ROOT_PDFS_PATH + id + '.pdf'
-            if pdfFilePath in alreadyDownloadedPDFs:
-                print('PDF has already been downloaded! Parsing!')
-                emails = parse_pdf(pdfFilePath)
-                datum['emails'] = emails
-                continue
+        pdfFilePath = ROOT_PDFS_PATH + id + '.pdf'
+        if pdfFilePath in alreadyDownloadedPDFs:
+            print('PDF has already been downloaded! Parsing!')
+            emails = parse_pdf(pdfFilePath)
+            datum['emails'] = emails
+            continue
 
 
-            initialURL = datum['initialURL']
-            print('InitialURL: ' + initialURL)
+        initialURL = datum['initialURL']
+        print('InitialURL: ' + initialURL)
 
-            finalURL = getMetaRefreshRedirectfinalURL(initialURL)
-            if finalURL:
-                datum['finalURL'] = finalURL
-                print('finalURL:' + finalURL)
+        finalURL = getMetaRefreshRedirectfinalURL(initialURL)
+        if finalURL:
+            datum['finalURL'] = finalURL
+            print('finalURL:' + finalURL)
 
-                # Make request to finalURL
-                try:
-                    response = requests.get(finalURL)
-                except Exception as e:
-                    print('Error sending request:')
-                    print(e)
-                except ConnectionResetError as e:
-                    print('ConnectionResetError!')
-                    print(e)
-                else:
-                    # If the response directly gave us a PDF, download it and associate the path to the research paper ID in our dict
-                    contentType, encoding = response.headers['content-type'].split(';')
-                    print(contentType)
-
-                    if contentType == 'application/pdf':
-                        print('Response is a PDF!')
-                        datum['downloadedPDFPath'] =  pdfFilePath
-                        with open(pdfFilePath, 'wb') as f:
-                            f.write(response.content)
-                            f.close()
-                        # Find the emails in the PDF we just downloaded
-                        emails = parse_pdf(pdfFilePath)
-                        datum['emails'] = emails
-
-                    if contentType == 'text/html':
-                        print('Response is another webpage!')
-                        newSplitWebpageURL = urllib.parse.urlparse(response.url);
-                        newWebpageBaseURL = newSplitWebpageURL.scheme + '://' + newSplitWebpageURL.netloc
-                        print('New webpage base url: ' + newWebpageBaseURL)
-                        pdfFilePath = scrapePDFDownloadButtonFromWebpage(response.content, datum, pdfFilePath, newWebpageBaseURL)
-                        if pdfFilePath:
-                            emails = parse_pdf(pdfFilePath)
-                            datum['emails'] = emails
-                        else:
-                            print('No PDF could be downloaded from webpage')
+            # Make request to finalURL
+            try:
+                response = requests.get(finalURL)
+            except Exception as e:
+                print('Error sending request:')
+                print(e)
+            except ConnectionResetError as e:
+                print('ConnectionResetError!')
+                print(e)
             else:
-                print('No final URL')
+                # If the response directly gave us a PDF, download it and associate the path to the research paper ID in our dict
+                contentType, encoding = response.headers['content-type'].split(';')
+                print(contentType)
+
+                if contentType == 'application/pdf':
+                    print('Response is a PDF!')
+                    datum['downloadedPDFPath'] =  pdfFilePath
+                    with open(pdfFilePath, 'wb') as f:
+                        f.write(response.content)
+                        f.close()
+                    # Find the emails in the PDF we just downloaded
+                    emails = parse_pdf(pdfFilePath)
+                    datum['emails'] = emails
+
+                if contentType == 'text/html':
+                    print('Response is another webpage!')
+
+                    # Try to extract email from webpage itself
+                    datum['emails'] = scrapeEmailsFromWebpage(response.content, datum)
+
+                    # Then try to extract email from any potential PDF links on the page
+                    newSplitWebpageURL = urllib.parse.urlparse(response.url);
+                    newWebpageBaseURL = newSplitWebpageURL.scheme + '://' + newSplitWebpageURL.netloc
+                    print('New webpage base url: ' + newWebpageBaseURL)
+                    pdfFilePath = scrapePDFDownloadButtonFromWebpage(response.content, datum, pdfFilePath, newWebpageBaseURL)
+                    if pdfFilePath:
+                        emails = parse_pdf(pdfFilePath)
+                        #combine emails from webpage and PDF
+                        datum['emails'] = combineListsRemoveDuplicates(datum['emails'], emails)
+                    else:
+                        print('Could not find PDF to be downloaded from webpage')
+
+                    if not datum['emails']:
+                        datum['emails'] = 'Could not find a PDF or emails'
+
+                # Make list of emails unique after we've tried extracting from everywhere
+                datum['emails'] = makeListUnique(datum['emails'])
+        else:
+            print('No final URL')
+            datum['emails'] = 'Could not find a PDF or emails'
 
     return researchPaperData
 
-
+def scrapeEmailsFromWebpage(webpageContent, datum):
+    soup = BeautifulSoup(webpageContent, 'lxml')
+    soup = soup.prettify('latin-1')
+    soupString = str(soup)
+    emails = find_emails(soupString)
+    return emails
 
 def scrapePDFDownloadButtonFromWebpage(webpageContent, datum, pdfFilePath, newWebpageBaseURL):
     soup = BeautifulSoup(webpageContent, 'lxml')
@@ -141,6 +160,12 @@ def scrapePDFDownloadButtonFromWebpage(webpageContent, datum, pdfFilePath, newWe
 def listToString(l):
     listToStr = ' '.join([str(elem) for elem in l])
     return listToStr
+
+def combineListsRemoveDuplicates(l1, l2):
+    return l1 + list(set(l2) - set(l1))
+
+def makeListUnique(l):
+    return list(dict.fromkeys(l))
 
 def downloadPDFFromWebpage(pdfURL, datum, pdfFilePath, newWebpageBaseURL):
     potentialFullURL = newWebpageBaseURL + pdfURL
